7bb7bcaa16e5eed0b28f0452570cba64431602e4
DanutIordache <danutiordache2001@gmail.com>
2025-06-26 11:29:20 +0300

    Prima versiune - Casa Tâmplarului Rustic

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import CasaTamplaruluiRustic from "./CasaTamplaruluiRustic";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <CasaTamplaruluiRustic />
+  </React.StrictMode>
+);
